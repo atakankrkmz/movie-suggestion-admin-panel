ca47230a71bd523d495a30f37ca4fb03a37fca3e
otokon58 <ata58kann@gmail.com>
2021-06-02 00:32:23 +0300

    CreateDirector component was become functional component

--- src/components/CreateDirector/index.js
+++ src/components/CreateDirector/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import axios from "axios";
 import { Helmet } from "react-helmet";
@@ -7,56 +7,14 @@ import { connect } from "react-redux";
 const mainurl = "https://moviesuggestionwebapi.azurewebsites.net/api/";
 const TITLE = "Add Director";
 
-class CreateDirector extends Component {
-  constructor(props) {
-    super(props);
+const CreateDirector = (props) => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [portre, setPortre] = useState("");
+  const [bornIn, setBornIn] = useState("");
+  const [bornAt, setBornAt] = useState(new Date());
 
-    this.onChangeDirectorname = this.onChangeDirectorname.bind(this);
-    this.onChangeDescription = this.onChangeDescription.bind(this);
-    this.onChangeDirectorImg = this.onChangeDirectorImg.bind(this);
-    this.onChangeBornIn = this.onChangeBornIn.bind(this);
-    this.onChangeBornAt = this.onChangeBornAt.bind(this);
-
-    this.onSubmit = this.onSubmit.bind(this);
-
-    this.state = {
-      directorname: "",
-      description: "",
-      directorimg: "",
-      bornIn: "",
-      bornAt: new Date(),
-    };
-  }
-
-  onChangeDirectorname(e) {
-    this.setState({
-      directorname: e.target.value,
-    });
-  }
-
-  onChangeDirectorImg(e) {
-    let file = e.target.files[0];
-
-    this.setState({ directorimg: file });
-  }
-
-  onChangeDescription(e) {
-    this.setState({
-      description: e.target.value,
-    });
-  }
-  onChangeBornIn(e) {
-    this.setState({
-      bornIn: e.target.value,
-    });
-  }
-  onChangeBornAt(e) {
-    this.setState({
-      bornAt: e,
-    });
-  }
-
-  formatDate = (date) => {
+  const formatDate = (date) => {
     var d = new Date(date),
       month = "" + (d.getMonth() + 1),
       day = "" + d.getDate(),
@@ -68,24 +26,18 @@ class CreateDirector extends Component {
     return [year, month, day].join("-");
   };
 
-  onSubmit(e) {
+  const onSubmit = (e) => {
     e.preventDefault();
 
-    let token = this.props.token;
-    let directorname = this.state.directorname;
-    let description = this.state.description;
-    let directorimg = this.state.directorimg;
-    let bornIn = this.state.bornIn;
-    let bornAt = this.formatDate(this.state.bornAt);
+    let token = props.token;
+    let formattedBornAt = formatDate(bornAt);
     let formdata = new FormData();
 
-    formdata.append("name", directorname);
+    formdata.append("name", name);
     formdata.append("description", description);
-    formdata.append("portre", directorimg);
+    formdata.append("portre", portre);
     formdata.append("bornIn", bornIn);
-    formdata.append("bornAt", bornAt);
-
-    console.log(formdata);
+    formdata.append("bornAt", formattedBornAt);
 
     axios({
       url: `${mainurl}directors/add`,
@@ -99,104 +51,96 @@ class CreateDirector extends Component {
       .then((response) => {
         console.log(response.data);
         alert("Director is added succesfully");
-        this.setState({
-          directorname: "",
-          description: "",
-          directorimg: "",
-          bornAt: new Date(),
-          bornIn: "",
-        });
       })
       .catch((err) => {
         console.log(err);
         alert("Something went wrong");
       });
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <Helmet>
-          <title>{TITLE}</title>
-        </Helmet>
-        <h3>Add new director</h3>
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <label>Director Name: </label>
-            <input
-              required
-              value={this.state.directorname}
-              onChange={this.onChangeDirectorname}
-              type="text"
-              class="form-control"
-              aria-describedby="helpId"
-              placeholder="Tim Burton"
-            />
-            <small id="helpId" class="form-text text-muted">
-              Insert the director's name
-            </small>
+  return (
+    <div>
+      <Helmet>
+        <title>{TITLE}</title>
+      </Helmet>
+      <h3>Add new director</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Director Name: </label>
+          <input
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            class="form-control"
+            aria-describedby="helpId"
+            placeholder="Tim Burton"
+          />
+          <small id="helpId" class="form-text text-muted">
+            Insert the director's name
+          </small>
+        </div>
+        <div className="form-group">
+          <label className="form-label" for="customFile">
+            Director Porte:{" "}
+          </label>
+          <input
+            type="file"
+            name="file"
+            class="form-control"
+            id="customFile"
+            onChange={(e) => {
+              let file = e.target.files[0];
+              setPortre(file);
+            }}
+          />
+        </div>
+        <div className="form-group">
+          <label>Born at: </label>
+          <div>
+            <DatePicker selected={bornAt} onChange={(e) => setBornAt(e)} />
           </div>
-          <div className="form-group">
-            <label className="form-label" for="customFile">
-              Director Porte:{" "}
-            </label>
-            <input
-              type="file"
-              name="file"
-              class="form-control"
-              id="customFile"
-              onChange={(e) => this.onChangeDirectorImg(e)}
-            />
-          </div>
-          <div className="form-group">
-            <label>Born at: </label>
-            <div>
-              <DatePicker
-                selected={this.state.bornAt}
-                onChange={this.onChangeBornAt}
-              />
-            </div>
-          </div>
-          <div className="form-group">
-            <label>Born in: </label>
-            <input
-              required
-              value={this.state.bornIn}
-              onChange={this.onChangeBornIn}
-              type="text"
-              class="form-control"
-              aria-describedby="helpId"
-              placeholder="Tim Burton"
-            />
-            <small id="helpId" class="form-text text-muted">
-              Insert the director's born date
-            </small>
-          </div>
-          <div className="form-group">
-            <label>Description: </label>
-            <input
-              value={this.state.description}
-              onChange={this.onChangeDescription}
-              type="text"
-              class="form-control"
-              aria-describedby="helpId"
-              placeholder="The director of movie which we first watched together"
-            />
-            <small id="helpId" class="form-text text-muted">
-              About the director
-            </small>
-          </div>
+        </div>
+        <div className="form-group">
+          <label>Born in: </label>
+          <input
+            required
+            value={bornIn}
+            onChange={(e) => setBornIn(e.target.value)}
+            type="text"
+            class="form-control"
+            aria-describedby="helpId"
+            placeholder="Tim Burton"
+          />
+          <small id="helpId" class="form-text text-muted">
+            Insert the director's born date
+          </small>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <input
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            type="text"
+            class="form-control"
+            aria-describedby="helpId"
+            placeholder="The director of movie which we first watched together"
+          />
+          <small id="helpId" class="form-text text-muted">
+            About the director
+          </small>
+        </div>
 
-          <div className="form-group">
-            <button type="submit" class="btn btn-primary">
-              Add Director
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+        <div className="form-group">
+          <button type="submit" class="btn btn-primary">
+            Add Director
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
 const mapStateToProps = (state) => {
   return {
     token: state.token,
