6bc5c04ae9f65b81d7b34f0f4c60783f0eb0c2cd
otokon58 <ata58kann@gmail.com>
2021-06-02 12:55:56 +0300

    CreateMovie component was become functional component & performance is better

--- src/components/CreateMovie/index.js
+++ src/components/CreateMovie/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import axios from "axios";
@@ -6,101 +6,42 @@ import { connect } from "react-redux";
 
 const mainurl = "https://moviesuggestionwebapi.azurewebsites.net/api/";
 
-class CreateMovie extends Component {
-  constructor(props) {
-    super(props);
+const CreateMovie = (props) => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [directorId, setDirectorId] = useState(0);
+  const [genreId, setGenreId] = useState(0);
+  const [poster, setPoster] = useState("");
+  const [boxOffice, setBoxOffice] = useState(0);
+  const [releaseDate, setReleaseDate] = useState(new Date());
+  const [genres, setGenres] = useState([]);
+  const [directors, setDirectors] = useState([]);
 
-    this.onChangeName = this.onChangeName.bind(this);
-    this.onChangeDescription = this.onChangeDescription.bind(this);
-    this.onChangeDirectorId = this.onChangeDirectorId.bind(this);
-    this.onChangeGenreId = this.onChangeGenreId.bind(this);
-    this.onChangePoster = this.onChangePoster.bind(this);
-    this.onChangeBoxOffice = this.onChangeBoxOffice.bind(this);
-    this.onChangeReleaseDate = this.onChangeReleaseDate.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-
-    this.state = {
-      name: "",
-      description: "",
-      directorId: 0,
-      genreId: 0,
-      poster: "",
-      boxOffice: 0,
-      releaseDate: new Date(),
-      genres: [],
-      directors: [],
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     axios
       .get(`${mainurl}directors/getall`)
       .then((response) => {
         if (response.data["data"].length > 0) {
-          this.setState({
-            directors: response.data["data"].map((director) => director),
-            directorId: response.data.data[0].id,
-          });
+          setDirectors(response.data["data"].map((director) => director));
+          setDirectorId(response.data.data[0].id);
         }
       })
       .catch((err) => console.log(err));
+  }, []);
 
+  useEffect(() => {
     axios
       .get(`${mainurl}genres/getall`)
       .then((response) => {
         if (response.data.length > 0) {
-          this.setState({
-            genres: response.data.map((genre) => genre),
-            genreId: response.data[0].id,
-          });
+          setGenres(response.data.map((genre) => genre));
+          setGenreId(response.data[0].id);
         }
       })
       .catch((err) => console.log(err));
-  }
+  }, []);
 
-  onChangeName(e) {
-    this.setState({
-      name: e.target.value,
-    });
-  }
-
-  onChangeDescription(e) {
-    this.setState({
-      description: e.target.value,
-    });
-  }
-
-  onChangeDirectorId(e) {
-    this.setState({
-      directorId: e.target.value,
-    });
-  }
-
-  onChangeGenreId(e) {
-    this.setState({
-      genreId: e.target.value,
-    });
-  }
-
-  onChangePoster(e) {
-    let file = e.target.files[0];
-
-    this.setState({ poster: file });
-  }
-
-  onChangeBoxOffice(e) {
-    this.setState({
-      boxOffice: e.target.value,
-    });
-  }
-
-  onChangeReleaseDate(date) {
-    this.setState({
-      releaseDate: date,
-    });
-  }
-
-  formatDate = (date) => {
+  const formatDate = (date) => {
     var d = new Date(date),
       month = "" + (d.getMonth() + 1),
       day = "" + d.getDate(),
@@ -112,17 +53,11 @@ class CreateMovie extends Component {
     return [year, month, day].join("-");
   };
 
-  onSubmit(e) {
+  const onSubmit = async (e) => {
     e.preventDefault();
 
-    let token = this.props.token;
-    let name = this.state.name;
-    let description = this.state.description;
-    let directorId = this.state.directorId;
-    let genreId = this.state.genreId;
-    let poster = this.state.poster;
-    let boxOffice = this.state.boxOffice;
-    let releaseDate = this.formatDate(this.state.releaseDate);
+    let token = props.token;
+    let formattedReleaseDate = formatDate(releaseDate);
     let formdata = new FormData();
 
     formdata.append("name", name);
@@ -131,11 +66,9 @@ class CreateMovie extends Component {
     formdata.append("genreId", genreId);
     formdata.append("posterFile", poster);
     formdata.append("boxOffice", boxOffice);
-    formdata.append("releaseDate", releaseDate);
+    formdata.append("releaseDate", formattedReleaseDate);
 
-    console.log(formdata);
-
-    axios({
+    await axios({
       url: `${mainurl}movies/add`,
       method: "POST",
       headers: {
@@ -152,126 +85,125 @@ class CreateMovie extends Component {
         console.log(err);
         alert("Something went wrong");
       });
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <h3>Add new movie</h3>
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <label>Movie Name: </label>
-            <input
-              value={this.state.name}
-              onChange={this.onChangeName}
-              type="text"
-              class="form-control"
-              aria-describedby="helpId"
-              placeholder="Big Fish"
-            />
-            <small id="helpId" class="form-text text-muted">
-              Insert the movie's name
-            </small>
-          </div>
-          <div className="form-group">
-            <label>Description: </label>
-            <input
-              value={this.state.description}
-              onChange={this.onChangeDescription}
-              type="text"
-              class="form-control"
-              aria-describedby="helpId"
-              placeholder="The movie which is watched first together"
-            />
-            <small id="helpId" class="form-text text-muted">
-              About the movie
-            </small>
-          </div>
-          <div className="form-group">
-            <label>Director Name: </label>
-            <select
-              ref="directorInput"
-              required
-              className="form-control"
-              value={this.state.directorId}
-              onChange={this.onChangeDirectorId}
-            >
-              {this.state.directors.map(function (director) {
-                return (
-                  <option key={director.id} value={director.id}>
-                    {" "}
-                    {director.name}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-          <div className="form-group">
-            <label>Genre: </label>
-            <select
-              ref="genreInput"
-              required
-              className="form-control"
-              value={this.state.genreId}
-              onChange={this.onChangeGenreId}
-            >
-              {this.state.genres.map(function (genre) {
-                return (
-                  <option key={genre.id} value={genre.id}>
-                    {" "}
-                    {genre.genreName}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-          <div className="form-group">
-            <label className="form-label" for="customFile">
-              Movie Poster:{" "}
-            </label>
-            <input
-              type="file"
-              name="file"
-              class="form-control"
-              id="customFile"
-              onChange={(e) => this.onChangePoster(e)}
-            />
-          </div>
+  return (
+    <div>
+      <h3>Add new movie</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Movie Name: </label>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            className="form-control"
+            aria-describedby="helpId"
+            placeholder="Big Fish"
+          />
+          <small id="helpId" className="form-text text-muted">
+            Insert the movie's name
+          </small>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <input
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            type="text"
+            className="form-control"
+            aria-describedby="helpId"
+            placeholder="The movie which is watched first together"
+          />
+          <small id="helpId" className="form-text text-muted">
+            About the movie
+          </small>
+        </div>
+        <div className="form-group">
+          <label>Director Name: </label>
+          <select
+            required
+            className="form-control"
+            value={directorId}
+            onChange={(e) => setDirectorId(e.target.value)}
+          >
+            {directors.map(function (director) {
+              return (
+                <option key={director.id} value={director.id}>
+                  {" "}
+                  {director.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Genre: </label>
+          <select
+            required
+            className="form-control"
+            value={genreId}
+            onChange={(e) => setGenreId(e.target.value)}
+          >
+            {genres.map(function (genre) {
+              return (
+                <option key={genre.id} value={genre.id}>
+                  {" "}
+                  {genre.genreName}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label className="form-label" htmlFor="customFile">
+            Movie Poster:{" "}
+          </label>
+          <input
+            type="file"
+            name="file"
+            className="form-control"
+            id="customFile"
+            onChange={(e) => {
+              let file = e.target.files[0];
+              setPoster(file);
+            }}
+          />
+        </div>
 
-          <div className="form-group">
-            <label>BoxOffice: </label>
-            <input
-              value={this.state.boxOffice}
-              onChange={this.onChangeBoxOffice}
-              type="text"
-              class="form-control"
-              aria-describedby="helpId"
-              placeholder="125"
-            />
-            <small id="helpId" class="form-text text-muted">
-              How much money did make the movie ?
-            </small>
-          </div>
+        <div className="form-group">
+          <label>BoxOffice: </label>
+          <input
+            value={boxOffice}
+            onChange={(e) => setBoxOffice(e.target.value)}
+            type="text"
+            className="form-control"
+            aria-describedby="helpId"
+            placeholder="125"
+          />
+          <small id="helpId" className="form-text text-muted">
+            How much money did make the movie ?
+          </small>
+        </div>
 
-          <div className="form-group">
-            <label>Release Date: </label>
-            <div>
-              <DatePicker
-                selected={this.state.releaseDate}
-                onChange={this.onChangeReleaseDate}
-              />
-            </div>
+        <div className="form-group">
+          <label>Release Date: </label>
+          <div>
+            <DatePicker
+              selected={releaseDate}
+              onChange={(date) => setReleaseDate(date)}
+            />
           </div>
-          <div className="form-group">
-            <button type="submit" class="btn btn-primary">
-              Add Movie
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary">
+            Add Movie
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
