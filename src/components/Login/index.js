f9786d9541156d191aaa16706892944c21136f1c
Atakan Korkmaz <ata58kann@gmail.com>
2021-06-28 23:48:28 +0300

    version changed

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -39,7 +39,7 @@ const LoginPage = (props) => {
             id="exampleInputPassword1"
           />
         </div>
-        <div><h4>v2.1</h4></div>
+        <div><h4>v2.2</h4></div>
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
